78caea00c60cc6c09251f6dd49ca80ba218f30d2
shambhu23 <77667416+shambhu23@users.noreply.github.com>
2021-04-07 21:40:55 +0530

    Update cb_buttons.py
    Updated

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -1,5 +1,4 @@
 import os
-
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
@@ -9,17 +8,3 @@ from plugins.commands import start, help
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 from translation import Translation
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-@Client.on_callback_query()
-async def button(bot, update):
-    if "|" in update.data:
-        await youtube_dl_call_back(bot, update)
-
-    elif "=" in update.data:
-        await ddl_call_back(bot, update)
-
-    elif update.data == "close":
-        await update.message.delete()
